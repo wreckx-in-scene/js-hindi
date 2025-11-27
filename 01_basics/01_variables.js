36127d871dd01c12c8c9642e3f04dcf132eb1e56
wreckx-in-scene <154344825+wreckx-in-scene@users.noreply.github.com>
2025-11-27 14:35:34 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 122567
+let accountEmail = "amoghdandotiya123@gmail.com"
+var accountPassword = "123456"
+let accountCity = "Morena"
+
+/*
+Prefer not to use var
+because of issue in block scope and funtional scope
+*/
+console.table([accountId , accountEmail , accountPassword , accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict";   //treat all JS code as newer version
+// alert(3+3) ----> we are using nodejs , not a browser to ts doesnt work
+
+let age = 21
+let isLoggesIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null =>standalone value
+// undefined => undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "Amogh")
+console.log(typeof age)
+console.log(typeof null)    // object
+console.log(typeof undefined)   // undefined
+
