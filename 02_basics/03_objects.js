6c8573fc28743cf68011a3c1eb33322bca88fdb4
wreckx-in-scene <154344825+wreckx-in-scene@users.noreply.github.com>
2025-12-01 18:44:28 +0000

    more on objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,9 +1,12 @@
 //object literals
 
-//using symbol in 
+//using symbol in objects
+const mySym = Symbol("key1")
+
 const JSUser = {
     name : "Amogh",
     age : 21,
+    [mySym] : "mykey1",
     email : "amogh123@gmail.com",
     location : "Kolkata",
     isLoggedIn : false,
@@ -12,3 +15,16 @@ const JSUser = {
 
 console.log(JSUser.email);     //one way of accessing data(not recommended)
 console.log(JSUser["email"]);   //preferred way
+console.log(typeof JSUser[mySym]);     //accessing symbol
+
+//freezing changes in objects 
+JSUser.name = "Amogh Dandotiya"
+Object.freeze(JSUser);
+console.log(JSUser);
+
+//declaring functions in objects 
+JSUser.greeting = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JSUser.greeting());
